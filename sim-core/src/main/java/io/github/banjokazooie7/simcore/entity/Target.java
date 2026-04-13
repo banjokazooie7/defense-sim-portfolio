b578d5706e224a201280499cd946aa3910f0a26d
banjokazooie7 <oanv7777@gmail.com>
2026-04-13 00:34:51 -0500

    add sim-core/src/main/java/io/github/banjokazooie7/simcore/entity/Target.java

---
+++ sim-core/src/main/java/io/github/banjokazooie7/simcore/entity/Target.java
@@ -0,0 +1,56 @@
+package io.github.banjokazooie7.simcore.entity;
+
+public final class Target implements Entity{
+    private final String id;
+    private Position2D position;
+    private double vxMetersPerSec;
+    private double vyMetersPerSec;
+
+    public Target(String id, Position2D startPosition, double vxMetersPerSec, double vyMetersPerSec){
+        if (id == null || id.isBlank()){
+            throw new IllegalArgumentException("Target id must be null or blank");
+        }
+        if(startPosition == null){
+            throw new IllegalArgumentException("Start position must not be null");
+        }
+        this.id = id;
+        this.position = startPosition;
+        this.vxMetersPerSec = vxMetersPerSec;
+        this.vyMetersPerSec = vyMetersPerSec;
+    }
+
+    @Override
+    public String getId(){return id;}
+
+    @Override
+    public Position2D getPosition(){return position;}
+    public double getVx(){return vxMetersPerSec;}
+    public double getVy(){return vyMetersPerSec;}
+
+    public double getSpeed(){return Math.sqrt(vxMetersPerSec * vxMetersPerSec 
+        + vyMetersPerSec * vyMetersPerSec);}
+
+    public void setVelocity(double vx, double vy){
+        this.vxMetersPerSec = vx;
+        this.vyMetersPerSec = vy;
+    }
+
+    public void update(long deltaMillis){
+        if(deltaMillis < 0){
+            throw new IllegalArgumentException("deltaMillis must be non-negative");//reject negative time
+        }
+        if(deltaMillis == 0) return; //if zero time, do nothing
+
+        double dtSec = deltaMillis / 1000.0;//convert millisecond to seconds
+        double dx = vxMetersPerSec * dtSec;//displacement = velocity * time
+        double dy = vyMetersPerSec * dtSec;
+        this.position = position.translate(dx, dy);
+    }
+
+    @Override
+    public String toString(){
+        return String.format("Target{id=%s, pos=%s, vel=(%.1f, %.1f) m/s}", 
+            id, position, vxMetersPerSec, vyMetersPerSec);
+    }
+
+}
